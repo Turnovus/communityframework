29bac99608bdeb62afe30d639f5e8791c911003e
ISOR3X <75855460+ISOR3X@users.noreply.github.com>
2021-06-24 22:06:28 +0200

    Reordering of files

---
+++ Source/communityframework/communityframework/CompUnlocksRecipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace CF
+{
+    class CompUnlocksRecipe : CompAffectedByFacilities
+    {
+        public CompProperties_UnlocksRecipe Props
+        {
+            get
+            {
+                return (CompProperties_UnlocksRecipe)this.props;
+            }
+        }
+    }   
+}
---
+++ Source/communityframework/communityframework/Harmony patches/PatchRunner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+
+namespace CF
+{
+    /// <summary>
+    /// This class patches all the Harmony classes
+    /// </summary>
+    [StaticConstructorOnStartup]
+    public class PatchRunner
+    {
+        static PatchRunner()
+        {
+            DoPatching();
+        }
+
+        public static void DoPatching()
+        {
+            //Start patching all harmony patches.
+            var harmony = new Harmony("com.communityframework.harmonypatches");
+            harmony.PatchAll();
+        }
+
+    }
+}
