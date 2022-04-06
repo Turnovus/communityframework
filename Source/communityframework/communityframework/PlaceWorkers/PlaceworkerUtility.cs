a87f3784f0f4d81372a4e3bff467356f637afa50
Turnovus <dinolover075@gmail.com>
2022-04-06 10:05:32 -0400

    Reformatted PlaceWorkers
    All of CuproPanda's PlaceWorkers and PlaceWorker utilities have been reformatted to fit within the 80-character line limit. I'm probably going to stop documenting every single method override because that requires more effort than it's really worth.
    
    Solution not rebuilt.

--- Source/communityframework/communityframework/PlaceWorkers/PlaceworkerUtility.cs
+++ Source/communityframework/communityframework/PlaceWorkers/PlaceworkerUtility.cs
@@ -7,32 +7,53 @@ using Verse;
 namespace CF
 {
     /// <summary>
-    /// Class holding utility functions for the various included <c>PlaceWorkers</c>.
+    /// Class holding utility functions for the various included
+    /// <c>PlaceWorkers</c>.
     /// </summary>
     class PlaceWorkerUtility
     {
         /// <summary>
-        /// Checks whether the building is a wall, namely that it holds a roof, blocks light, and covers the floor.
+        /// Checks whether the building is a wall, namely that it holds a roof,
+        /// blocks light, and covers the floor.
         /// </summary>
         /// <param name="building">The building to check.</param>
-        /// <returns>Whether the building is a wall by the above criteria.</returns>
+        /// <returns>
+        /// Whether the building is a wall by the above criteria.
+        /// </returns>
         public static bool IsWall(Building building)
         {
             ThingDef def = building?.def;
-            return def != null && (def.holdsRoof && def.blockLight && def.coversFloor);
+            return def != null &&
+                def.holdsRoof && def.blockLight && def.coversFloor;
         }
+
         /// <summary>
-        /// Returns whether an identical <c>Thing</c> exists in the specified cell, namely that its <c>Def</c> matches and it's facing in the same direction.
+        /// Returns whether an identical <c>Thing</c> exists in the specified
+        /// cell, namely that its <c>Def</c> matches and it's facing in the
+        /// same direction.
         /// </summary>
-        /// <param name="buildableDef">The <c>BuildableDef</c> a <c>PlaceWorker</c> is trying to place.</param>
+        /// <param name="buildableDef">
+        /// The <c>BuildableDef</c> a <c>PlaceWorker</c> is trying to place.
+        /// </param>
         /// <param name="cell">The cell it's trying to be placed in.</param>
-        /// <param name="rot">The rotation of the current thing which is attempting to be placed.</param>
+        /// <param name="rot">
+        /// The rotation of the current thing which is attempting to be placed.
+        /// </param>
         /// <param name="map">The map it's trying to be placed in.</param>
         /// <returns></returns>
-        public static bool ConflictingThing(BuildableDef buildableDef, IntVec3 cell, Rot4 rot, Map map)
+        public static bool ConflictingThing(
+            BuildableDef buildableDef,
+            IntVec3 cell,
+            Rot4 rot,
+            Map map
+        )
         {
-            List<Thing> things = map.thingGrid.ThingsListAtFast(cell);            
-            foreach (Thing t in things) if (t.def as BuildableDef == buildableDef && t.Rotation == rot) return true; 
+            List<Thing> things = map.thingGrid.ThingsListAtFast(cell);
+            foreach (Thing t in things)
+            {
+                if (t.def == buildableDef && t.Rotation == rot)
+                    return true;
+            }
             return false;
         }
     }
