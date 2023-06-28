6ab5df3d0ea32e24d622e2fdcd55d92950035cc8
Turnovus <dinolover075@gmail.com>
2023-06-28 05:32:17 -0400

    Removed unused imports

--- Source/communityframework/communityframework/Harmony patches/BatteryChargePatches.cs
+++ Source/communityframework/communityframework/Harmony patches/BatteryChargePatches.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
-using Verse;
 using RimWorld;
 using HarmonyLib;
 
