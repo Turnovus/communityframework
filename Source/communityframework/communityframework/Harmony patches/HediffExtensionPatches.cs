c799d2f08d8c2d0126d7f0a5de8ecdc90312f93d
Turnovus <dinolover075@gmail.com>
2025-08-02 21:32:01 -0400

    Add HediffExtension data to hediff display stats

--- Source/communityframework/communityframework/Harmony patches/HediffExtensionPatches.cs
+++ Source/communityframework/communityframework/Harmony patches/HediffExtensionPatches.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using HarmonyLib;
+using RimWorld;
 using Verse;
 
 namespace CF
@@ -6,6 +11,9 @@ namespace CF
     [ClassWithPatches("ApplyHediffExtensionPatches")]
     static class HediffExtensionPatches
     {
+        // Use this to store context that HediffStage does not have access to
+        private static HediffDef viewingDef;
+        
         [HarmonyPatch(typeof(HediffGiver))]
         [HarmonyPatch(nameof(HediffGiver.TryApply))]
         public static class TryApply
@@ -27,6 +35,56 @@ namespace CF
                 }
                 
                 return true;
+            }
+        }
+        
+        /// <summary>
+        /// This patch stores the HediffDef for which HediffDef.SpecialDisplayStats is being invoked, so that we can
+        /// reference it in the HediffStage.SpecialDisplayStats patch. This cache is NOT cleared, and must be cleared
+        /// by the consumer for safety.
+        /// </summary>
+        [HarmonyPatch(typeof(HediffDef))]
+        [HarmonyPatch(nameof(HediffDef.SpecialDisplayStats))]
+        static class HediffDefDisplayStats
+        {
+            [HarmonyPrefix]
+            public static void StoreContext(ref HediffDef __instance) => viewingDef = __instance;
+        }
+        
+        [HarmonyPatch(typeof(HediffStatsUtility))]
+        [HarmonyPatch(nameof(HediffStatsUtility.SpecialDisplayStats))]
+        static class HediffStageDisplayStats
+        {
+            [HarmonyPostfix]
+            public static IEnumerable<StatDrawEntry> AddBlockedGiverHediffs(IEnumerable<StatDrawEntry> values, Hediff instance)
+            {
+                
+                if (values != null)
+                    foreach (StatDrawEntry entry in values)
+                        yield return entry;
+                
+                // This matches vanilla logic of not displaying 
+                HediffDef hediffDef = instance?.def ?? viewingDef;
+                HediffExtension extension = hediffDef?.GetModExtension<HediffExtension>();
+                
+                // Clear the viewingDef cache now that we've had a chance to store its value locally
+                viewingDef = null;
+                
+                if (extension?.hediffGiversCannotGiveByStage == null)
+                    yield break;
+                
+                // We don't need to know the stage of the active hediff because vanilla only runs the base method for
+                // Hediffs that have exactly one stage. An index of 0 is most vanilla-accurate here.
+                List<HediffDef> blockedHediffs = extension.hediffGiversCannotGiveByStage[0];
+                if (!blockedHediffs.NullOrEmpty())
+                {
+                    yield return new StatDrawEntry(
+                        StatCategoryDefOf.CapacityEffects,
+                        "PreventsInfection".Translate(),
+                        blockedHediffs.Select(im => im.label).ToCommaList().CapitalizeFirst(),
+                        "Stat_Hediff_PreventsInfection_Desc".Translate(),
+                        4050);
+                }
             }
         }
     }
